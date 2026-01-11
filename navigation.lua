1c257e8c87bac1ca7cc7d143fc94099e4297bd36
Zhengnan Zhao <17503044+zzhengnan@users.noreply.github.com>
2026-01-10 23:04:36 -0500

    Add ctrl + shift + o/i

--- navigation.lua
+++ navigation.lua
@@ -11,6 +11,23 @@ function module.apply_to_config(config)
 		-- Move between tabs
 		{ key = "h", mods = "ALT", action = wezterm.action.ActivateTabRelative(-1) },
 		{ key = "l", mods = "ALT", action = wezterm.action.ActivateTabRelative(1) },
+		-- Move between left/right panes and zoom
+		{
+			key = "i",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.Multiple({
+				wezterm.action.ActivatePaneDirection("Left"),
+				wezterm.action.TogglePaneZoomState,
+			}),
+		},
+		{
+			key = "o",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.Multiple({
+				wezterm.action.ActivatePaneDirection("Right"),
+				wezterm.action.TogglePaneZoomState,
+			}),
+		},
 	}
 	for _, key_bind in ipairs(key_binds) do
 		table.insert(config.keys, key_bind)
--- wezterm.lua
+++ wezterm.lua
@@ -2,13 +2,14 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	config.default_prog = { "C:\\Program Files\\Git\\bin\\bash.exe", "-li" }
+	config.default_prog = { "C:\\Program Files\\Git\\bin\\bash.exe", "-i" }
 	config.font_size = 10
 else
-	config.font_size = 14
+	config.font_size = 16
 end
 
-config.color_scheme = "Dark+"
+config.color_scheme = "tokyonight"
+config.default_cursor_style = "BlinkingBar"
 config.font = wezterm.font("JetBrains Mono")
 config.window_background_opacity = 0.9
 config.window_decorations = "RESIZE" -- Remove window title
