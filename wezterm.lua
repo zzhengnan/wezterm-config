beb9ea752a8065e5fdde293934de8c49eeef0aed
Zhengnan Zhao <17503044+zzhengnan@users.noreply.github.com>
2026-02-22 09:56:03 -0500

    Customize tab title

--- wezterm.lua
+++ wezterm.lua
@@ -11,6 +11,11 @@ end
 config.color_scheme = "tokyonight"
 config.default_cursor_style = "BlinkingBar"
 config.font = wezterm.font("JetBrains Mono")
+config.inactive_pane_hsb = {
+	-- Decrease brightness for inactive panes
+	brightness = 0.2,
+}
+config.use_fancy_tab_bar = true
 config.window_background_opacity = 0.9
 config.window_decorations = "RESIZE" -- Remove window title
 
@@ -21,12 +26,22 @@ config.leader = { key = "Space", mods = "SHIFT", timeout_milliseconds = 1000 }
 config.keys = {}
 config.key_tables = {}
 
--- Add left/right padding to widen tabs
+-- Custom tab title: Current working directory plus # of panes in current tab, with padding on either side
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	-- TODO: Don't use full path
+	local title = tab.active_pane.current_working_dir.file_path
+
+	-- Use `mux` to handle pane count correctly for when a pane is zoomed in
+	local num_panes = #wezterm.mux.get_tab(tab.tab_id):panes()
+
 	local pad = "          "
-	return pad .. tab.active_pane.title .. pad
+	title = pad .. title .. pad
+	if num_panes > 1 then
+		title = title .. num_panes
+	end
+
+	return title
 end)
-config.use_fancy_tab_bar = true
 
 require("navigation").apply_to_config(config)
 require("pane").apply_to_config(config)
